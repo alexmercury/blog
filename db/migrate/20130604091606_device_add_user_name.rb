73794266e248c37630eef0d8a7de6fb202f59687
alexsummers <alexsummers@yandex.ua>
2013-06-04 13:00:21 +0300

    add user name & style

---
+++ db/migrate/20130604091606_device_add_user_name.rb
@@ -0,0 +1,9 @@
+class DeviceAddUserName < ActiveRecord::Migration
+  def up
+    add_column :users, :user_name , :string
+  end
+
+  def down
+    remove_column :users, :user_name , :string
+  end
+end
