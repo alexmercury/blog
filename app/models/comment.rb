b8ff9a3a48f736beedda4882dba209ac6213b3bb
alexsummers <alexsummers@yandex.ua>
2013-06-17 13:32:31 +0300

    delete

--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < ActiveRecord::Base
-  attr_accessible :post_id, :text
-  
-	belongs_to :post, :counter_cache => true
-  belongs_to :user,  :counter_cache => true
-	
-	validates_presence_of :text
-end
--- app/models/posts_to_tags.rb
+++
@@ -1,3 +0,0 @@
-class PostsToTags < ActiveRecord::Base
-  attr_accessible :id_post, :id_tag
-end
